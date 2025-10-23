45a6c1f5ee7ea9af3b66ba56252b7e886d99f625
jreleaserbot <jreleaser@kordamp.org>
2025-10-23 12:30:42 +0000

    doc-exec v1.0.0

--- Formula/doc-exec.rb
+++ Formula/doc-exec.rb
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.17.0 at 2025-10-23T11:40:31.184030968Z
+# Generated with JReleaser 1.17.0 at 2025-10-23T12:30:37.823776394Z
 
 class DocExec < Formula
   desc "ComboStrap Doc Exec"
